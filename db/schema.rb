1c46512355b06c1e926ff287154b2370e86ec7d6
neto <hatreun@gmail.com>
2015-03-08 06:13:40 -0600

    devise again

--- db/schema.rb
+++ db/schema.rb
@@ -11,144 +11,157 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150308103820) do
+ActiveRecord::Schema.define(version: 20150308121004) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "expertices", primary_key: "experticeid", force: :cascade do |t|
+  create_table "expert_reviews", force: :cascade do |t|
+    t.string   "message",    limit: 120
+    t.integer  "score"
+    t.datetime "created_at"
+    t.integer  "project_id",             null: false
+    t.integer  "user_id",                null: false
+    t.integer  "status_id"
+  end
+
+  create_table "expert_users", id: false, force: :cascade do |t|
+    t.integer  "preference_id", null: false
+    t.integer  "user_id",       null: false
+    t.integer  "reviewer_id"
+    t.datetime "reviewer_at"
+    t.integer  "status_id",     null: false
+  end
+
+  create_table "expertices", force: :cascade do |t|
     t.string  "name",          limit: 40
-    t.integer "preferencesid",            null: false
+    t.integer "preference_id",            null: false
   end
 
-  create_table "preferences", primary_key: "preferencesid", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table "patterns", force: :cascade do |t|
+    t.string "name", limit: 30
   end
 
-  create_table "preferences_project", id: false, force: :cascade do |t|
-    t.integer "projectid",     null: false
-    t.integer "preferencesid", null: false
+  create_table "preferences", force: :cascade do |t|
+    t.string   "name",       limit: 40
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "profiles", primary_key: "profileid", force: :cascade do |t|
-    t.string "profile", limit: 40
+  create_table "preferences_projects", id: false, force: :cascade do |t|
+    t.integer "project_id",    null: false
+    t.integer "preference_id", null: false
   end
 
-  create_table "projects", primary_key: "projectid", force: :cascade do |t|
-    t.string   "project_name",        limit: 120
-    t.string   "scope",               limit: 400
-    t.string   "description",         limit: 200
-    t.datetime "created_at",                      precision: 6
-    t.datetime "updated_at",                      precision: 6
-    t.integer  "userid",                                        null: false
-    t.integer  "user_validate_id",                              null: false
-    t.integer  "proyect_typesid",                               null: false
-    t.datetime "validate_at",                     precision: 6
-    t.integer  "validation_statusid",                           null: false
-    t.integer  "invalidate_typeid",                             null: false
-    t.integer  "statusid"
+  create_table "profiles", force: :cascade do |t|
+    t.string "profile", limit: 30
+  end
+
+  create_table "project_types", force: :cascade do |t|
+    t.string  "type",      limit: 40
+    t.integer "status_id"
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "name",                 limit: 120
+    t.string   "scope",                limit: 400
+    t.string   "description",          limit: 200
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id",                          null: false
+    t.integer  "user_validate_id",                 null: false
+    t.integer  "project_types_id",                 null: false
+    t.datetime "validate_at"
+    t.integer  "validation_status_id",             null: false
+    t.integer  "invalidate_type_id",               null: false
+    t.integer  "status_id"
     t.integer  "proposed_time"
     t.integer  "step_01"
     t.integer  "step_02"
+    t.integer  "step_03"
     t.integer  "total_percent"
     t.integer  "goal"
-    t.datetime "goal_time",                       precision: 6
+    t.datetime "goal_time"
   end
 
-  create_table "proyect_types", primary_key: "proyect_typesid", force: :cascade do |t|
-    t.string  "proyect_types", limit: 40
-    t.integer "statusid"
-  end
-
-  create_table "status", primary_key: "statusid", force: :cascade do |t|
-    t.integer "type"
+  create_table "status", force: :cascade do |t|
     t.string  "name", limit: 30
+    t.integer "type"
   end
 
-  create_table "users", primary_key: "userid", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name",             limit: 30
+    t.string   "last_name",              limit: 40
+    t.string   "address",                limit: 400
+    t.string   "sex",                    limit: 30
+    t.string   "facebook",               limit: 40
+    t.string   "twitter",                limit: 40
+    t.string   "zip",                    limit: 10
+    t.datetime "birthday"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "status_id",                                       null: false
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "email",                              default: "", null: false
+    t.string   "encrypted_password",                 default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",                      default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "facebook"
-    t.string   "twitter"
-    t.string   "zip"
-    t.integer  "statusid"
-    t.string   "provider"
-    t.string   "uid"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
-  create_table "users_cluster", id: false, force: :cascade do |t|
-    t.integer  "preferencesid",               null: false
-    t.integer  "userid",                      null: false
-    t.integer  "reviewer_id"
-    t.datetime "reviewer_at",   precision: 6
-    t.integer  "statusid",                    null: false
-  end
-
   create_table "users_expertices", id: false, force: :cascade do |t|
-    t.integer "experticeid", null: false
-    t.integer "userid",      null: false
+    t.integer "expertice_id", null: false
+    t.integer "user_id",      null: false
   end
 
   create_table "users_preferences", id: false, force: :cascade do |t|
-    t.integer "userid",        null: false
-    t.integer "preferencesid", null: false
+    t.integer "user_id",       null: false
+    t.integer "preference_id", null: false
   end
 
   create_table "users_profiles", id: false, force: :cascade do |t|
-    t.integer "profileid", null: false
-    t.integer "userid",    null: false
+    t.integer "profile_id", null: false
+    t.integer "user_id",    null: false
   end
 
   create_table "users_projects", id: false, force: :cascade do |t|
-    t.integer  "projectid",                null: false
-    t.integer  "userid",                   null: false
-    t.datetime "created_at", precision: 6
+    t.integer  "project_id", null: false
+    t.integer  "user_id",    null: false
+    t.datetime "created_at"
   end
 
-  create_table "users_reviews", primary_key: "users_reviewid", force: :cascade do |t|
-    t.string   "message",    limit: 120
-    t.integer  "score"
-    t.datetime "created_at",             precision: 6
-    t.integer  "projectid",                            null: false
-    t.integer  "userid",                               null: false
-  end
-
-  add_foreign_key "expertices", "preferences", column: "preferencesid", primary_key: "preferencesid", name: "refpreferences271"
-  add_foreign_key "preferences_project", "preferences", column: "preferencesid", primary_key: "preferencesid", name: "refpreferences181"
-  add_foreign_key "preferences_project", "projects", column: "projectid", primary_key: "projectid", name: "refprojects171"
-  add_foreign_key "projects", "status", column: "invalidate_typeid", primary_key: "statusid", name: "refstatus301"
-  add_foreign_key "projects", "status", column: "statusid", primary_key: "statusid", name: "refstatus101"
-  add_foreign_key "projects", "status", column: "validation_statusid", primary_key: "statusid", name: "refstatus121"
-  add_foreign_key "projects", "users", column: "user_validate_id", primary_key: "userid", name: "refusers51"
-  add_foreign_key "projects", "users", column: "userid", primary_key: "userid", name: "refusers41"
-  add_foreign_key "users_cluster", "preferences", column: "preferencesid", primary_key: "preferencesid", name: "refpreferences231"
-  add_foreign_key "users_cluster", "status", column: "statusid", primary_key: "statusid", name: "refstatus281"
-  add_foreign_key "users_cluster", "users", column: "userid", primary_key: "userid", name: "refusers241"
-  add_foreign_key "users_expertices", "expertices", column: "experticeid", primary_key: "experticeid", name: "refexpertices251"
-  add_foreign_key "users_expertices", "users", column: "userid", primary_key: "userid", name: "refusers261"
-  add_foreign_key "users_preferences", "preferences", column: "preferencesid", primary_key: "preferencesid", name: "refpreferences221"
-  add_foreign_key "users_preferences", "users", column: "userid", primary_key: "userid", name: "refusers201"
-  add_foreign_key "users_profiles", "profiles", column: "profileid", primary_key: "profileid", name: "refprofiles151"
-  add_foreign_key "users_profiles", "users", column: "userid", primary_key: "userid", name: "refusers161"
-  add_foreign_key "users_projects", "projects", column: "projectid", primary_key: "projectid", name: "refprojects21"
-  add_foreign_key "users_projects", "users", column: "userid", primary_key: "userid", name: "refusers31"
-  add_foreign_key "users_reviews", "projects", column: "projectid", primary_key: "projectid", name: "refprojects71"
-  add_foreign_key "users_reviews", "users", column: "userid", primary_key: "userid", name: "refusers81"
+  add_foreign_key "expert_reviews", "projects", name: "expert_reviews_project_id_fkey"
+  add_foreign_key "expert_reviews", "status", name: "expert_reviews_status_id_fkey"
+  add_foreign_key "expert_reviews", "users", name: "expert_reviews_user_id_fkey"
+  add_foreign_key "expert_users", "preferences", name: "expert_users_preference_id_fkey"
+  add_foreign_key "expert_users", "status", name: "expert_users_status_id_fkey"
+  add_foreign_key "expert_users", "users", name: "expert_users_user_id_fkey"
+  add_foreign_key "expertices", "preferences", name: "expertices_preference_id_fkey"
+  add_foreign_key "preferences_projects", "preferences", name: "preferences_projects_preference_id_fkey"
+  add_foreign_key "preferences_projects", "projects", name: "preferences_projects_project_id_fkey"
+  add_foreign_key "project_types", "status", name: "project_types_status_id_fkey"
+  add_foreign_key "projects", "project_types", column: "project_types_id", name: "projects_project_types_id_fkey"
+  add_foreign_key "projects", "status", column: "invalidate_type_id", name: "projects_invalidate_type_id_fkey"
+  add_foreign_key "projects", "status", column: "validation_status_id", name: "projects_validation_status_id_fkey"
+  add_foreign_key "projects", "status", name: "projects_status_id_fkey"
+  add_foreign_key "projects", "users", column: "user_validate_id", name: "projects_user_validate_id_fkey"
+  add_foreign_key "projects", "users", name: "projects_user_id_fkey"
+  add_foreign_key "users", "status", name: "users_status_id_fkey"
+  add_foreign_key "users_expertices", "expertices", name: "users_expertices_expertice_id_fkey"
+  add_foreign_key "users_expertices", "users", name: "users_expertices_user_id_fkey"
+  add_foreign_key "users_preferences", "preferences", name: "users_preferences_preference_id_fkey"
+  add_foreign_key "users_preferences", "users", name: "users_preferences_user_id_fkey"
+  add_foreign_key "users_profiles", "profiles", name: "users_profiles_profile_id_fkey"
+  add_foreign_key "users_profiles", "users", name: "users_profiles_user_id_fkey"
+  add_foreign_key "users_projects", "projects", name: "users_projects_project_id_fkey"
+  add_foreign_key "users_projects", "users", name: "users_projects_user_id_fkey"
 end
